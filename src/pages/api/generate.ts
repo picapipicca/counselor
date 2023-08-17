faae93eae436fd74866284a9ac3efc4ab0584a58
HAEWON LEE <90237570+picapipicca@users.noreply.github.com>
2023-08-17 19:34:43 +0900

    git commit_

---
+++ src/pages/api/generate.ts
@@ -0,0 +1,38 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import openai from "@/utils/openai";
+
+type Data = {
+  name: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  const body = JSON.parse(req.body);
+  const { question } = body || {};
+
+  console.log("0", body.question);
+  const completion = await openai.createChatCompletion({
+    model:
+      "gpt-3.5-turbo",
+    messages:
+      [
+        {
+          role: "user",
+          content: `${question}`
+        },
+        {
+          role: "system",
+          content: "you are a dating counselor for couples. please answer the following questions in korean in 5 options in a nutshell. and recap the advice with keywords. "
+        }
+      ],
+    max_tokens: 400,
+    temperature: 0.7
+
+  });
+  const botAnswer = completion.data.choices[0].message.content;
+  console.log("1", botAnswer);
+  res.status(200).json({ botAnswer });
+}
