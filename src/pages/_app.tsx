d8dab778912b06cec7bc17930d38e12dc1076115
HAEWON LEE <90237570+picapipicca@users.noreply.github.com>
2023-08-25 02:33:34 +0900

    Layout, redux provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
+import Layout from '@/components/Layout'
+import { Providers } from '@/redux/provider'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Providers>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Providers>
+  )
+
 }
