56e65bcc4fe59f3f2e83680cd10d192cf81d0a09
HAEWON LEE <90237570+picapipicca@users.noreply.github.com>
2023-08-25 02:30:16 +0900

    new:react hook form

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,18 @@
 import { Controller } from "react-hook-form";
-
+import { ErrorMessage } from "@hookform/error-message";
+import { useWatch } from "react-hook-form";
 interface CardProps {
     index: number;
     section: any;
     control: any;
+    errors?: any;
 }
 
-const Card = ({ control, section, index }: CardProps) => {
-
+const Card = ({ control, section, index, errors }: CardProps) => {
+    const results = useWatch({ control, name: "question" })
+    console.log(results)
     return (
-        <section className="mx-auto sm:max-w-xl w-full p-10 bg-white border border-gray-200 rounded-lg shadow sm:p-10">
+        <section className="mx-auto sm:max-w-2xl w-full px-6 py-10 bg-white border border-gray-200 rounded-lg shadow sm:p-10 sm:h-[45vh] h-[50vh]">
             <h3 className="mb-3 text-xl font-medium text-gray-900 md:text-xl"> <span className="text-orange-600">Q.</span>&nbsp; {section.title}</h3>
             <div className="py-6 space-y-3">
                 <p>본인</p>
@@ -17,25 +20,62 @@ const Card = ({ control, section, index }: CardProps) => {
                     control={control}
                     name={`question.${index}.me`}
                     render={({ field: { value, onChange, ...fieldProps } }) => (
-                        <div className="flex space-x-4"  >
-                            {section.answerList.map((el: string, idx: number) => <button className="border border-gray-200 px-4 outline_btn" value={el} {...fieldProps} onClick={() => onChange(el)}
-                                key={idx}>{el}</button>)}
-                        </div>
+                        <>
+                            <div className="grid grid-flow-col auto-cols-auto sm:flex sm:space-x-4 sm:justify-around">
+                                {section.answerList.map((el: string, idx: number) =>
+                                    <button
+                                        className={`${results.find((el: any) => el.id === section.id).me === el ? "bg-gray-100 text-blue-700" : "bg-white text-gray-900"} shrink border border-gray-200 sm:py-4 px-1 primary_btn`}
+                                        value={el} {...fieldProps}
+                                        onClick={() => onChange(el)}
+                                        key={idx}>{el}
+                                    </button>)}
+                            </div>
+                        </>
                     )}
                 />
+
+                {errors && <ErrorMessage
+                    errors={errors}
+                    name={`question.${index}.me`}
+                    render={({ message }: any) => (
+                        <div className="text-right absolute right-0">
+                            <p className="text-red-500 text-base leading-3.5 mb-0 my-1">
+                                {message}
+                            </p>
+                        </div>
+                    )}
+                />}
 
                 <p>상대방</p>
                 <Controller
                     control={control}
                     name={`question.${index}.you`}
                     render={({ field: { value, onChange, ...fieldProps } }) => (
-                        <div className="flex space-x-4">
-                            {section.answerList.map((el: string, idx: number) => <button className="border border-gray-200 px-4 outline_btn" value={el} {...fieldProps} onClick={() => onChange(el)}
-                                key={idx}>{el}</button>)}
-                        </div>
+                        <>
+                            <div className="grid grid-flow-col auto-cols-auto sm:flex sm:space-x-4 sm:justify-around">
+                                {section.answerList.map((el: string, idx: number) =>
+                                    <button
+                                        className={`${results.find((el: any) => el.id === section.id).you === el ? "bg-gray-100 text-blue-700" : "bg-white text-gray-900"} shrink border border-gray-200 sm:py-4 px-1 primary_btn`}
+                                        value={el} {...fieldProps}
+                                        onClick={() => onChange(el)}
+                                        key={idx}>{el}
+                                    </button>)}
+                            </div>
+                        </>
                     )}
                 />
             </div>
+            {errors && <ErrorMessage
+                errors={errors}
+                name={`question.${index}.me`}
+                render={({ message }: any) => (
+                    <div className="text-right absolute right-0">
+                        <p className="text-red-500 text-base leading-3.5 mb-0 my-1">
+                            {message}
+                        </p>
+                    </div>
+                )}
+            />}
         </section>
     )
 }
--- src/pages/questions/index.tsx
+++ src/pages/questions/index.tsx
@@ -1,7 +1,16 @@
 import React, { useState } from "react";
+import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
+// import { useAppDispatch } from "@/redux/hook";
+// import { storeQuestion } from "@/redux/resSlice"
 import { questionArr } from "@/utils/dummy";
 import Card from "@/components/Card";
+import useTransText from "@/utils/transText";
+import { createParser } from "eventsource-parser";
+import { Song_Myung } from "next/font/google";
+import Image from "next/image";
+
+const song = Song_Myung({ weight: "400", subsets: ['latin'], })
 
 type FormValues = {
     question: any[];
@@ -9,91 +18,124 @@ type FormValues = {
 }
 
 const QuestionPage = () => {
+
     const [currentIdx, setCurrentIdx] = useState(0);
-    const [answer, setAnswer] = useState<any>();
-    
-    const onGenerate = async () => {
-        setAnswer(undefined)
-        const { botAnswer } = await fetch('/api/generate', {
-            method: 'POST',
-            //answer 자리에 받은 데이터 넣어주기
-            body: JSON.stringify({ answer })
-        }).then(res => res.json());
-        setAnswer(botAnswer);
-    }
+    const [inputBox, setInputBox] = useState(false);
+    const [answer, setAnswer] = useState('');
+    const { transQuestion } = useTransText()
+
     const form = useForm<FormValues>({
         defaultValues: {
             addText: "",
-            question: [
-                { me: "", you: "" },
-                { me: "", you: "" },
-                { me: "", you: "" },
-                { me: "", you: "" },
-                { me: "", you: "" },
-                { me: "", you: "" },
-                { me: "", you: "" },
-                { me: "", you: "" },
-                { me: "", you: "" },
-                { me: "", you: "" },
-                { me: "", you: "" },
-                { me: "", you: "" },
-                { me: "", you: "" },
-                { me: "", you: "" },]
+            question: questionArr.map((el: any) => ({ id: el.id, me: "", you: "" }))
         }
     })
     const { handleSubmit, register, control, formState: { errors } } = form;
+
     const onSubmit = (data: any) => {
-        console.log(data);
+        const answerInLine = transQuestion(questionArr, data.question, data.addText);
+        onGenerate(answerInLine)
+        // dispatch(storeQuestion({ answerInLine }))
+        // router.push('/questions/result');
     }
-    //onGenerate 어디서 해서 question을 어디로 보내고 생성할지?
-    // 1. 마지막으로 카드에 인풋 넣어서 더 하고싶은말 적게하고,-완료 
-    // array로 받아온것을 다시 잘 정리해서 (utils에서 작성 에픽 여,남 받아오는것에 따라 바뀌는것 참고) 한줄로 만들어서 onGenerate - onSubmtit 에서 작성하기
-    // generate된 queston을 신상정보를 데이터로 저장 
-    // 2. 로딩중에는 주고 사진을 랜덤으로 보여주기
-    // 3. result 를 컴포넌트 조립해서 보여주기 (부적, 따다닥 나타나기 등)
-    // 4. react-hook-form 다시 도전해보기
-    // 어드민 페이지
-    // 구상 : questionArr를 어드민에서 관리한다 어드민에서 가지고오기
-    // 질문 장르에 따라 버튼을 만들어서 (어드민에서 만들면 생기게) 들어갈수있게
-    //예: 관계 유지, 관계 이별, 사랑을 갈구, 등등 그 mei 페이지 다시 들어가보기
-    // global css 에 apply error남
 
     const goBack = (idx: number) => {
         setCurrentIdx(idx - 1);
+        setInputBox(false);
     }
     const onSwipe = (direction: any) => {
         if (currentIdx >= 0 && currentIdx < questionArr.length - 1) {
             setCurrentIdx(currentIdx + 1);
         }
         if (direction === 'result') {
-            // onGenerate()
+            setInputBox(true);
+        }
+    }
+
+    const onGenerate = async (userAnswer: string) => {
+
+        const response = await fetch('/api/chat-stream', {
+            method: "POST",
+            body: JSON.stringify({ userAnswer }),
+        })
+
+        const reader = response.body?.getReader();
+        const decoder = new TextDecoder();
+
+        function onParse(event: any) {
+            if (event.type === 'event') {
+                try {
+                    const data = JSON.parse(event.data);
+                    data.choices
+                        .filter(({ delta }: any) => !!delta.content)
+                        .forEach(({ delta }: any) => {
+                            setAnswer((prev: any) => {
+                                return `${prev || ''}${delta.content}`
+                            });
+
+                        })
+                } catch (e) {
+                    console.log(e);
+                }
+            }
+        }
+        const parser = createParser(onParse);
+
+        while (true && reader) {
+            const { value, done } = await reader.read();
+            const dataString = decoder.decode(value);
+            if (done || dataString.includes('[DONE]')) break;
+            parser.feed(dataString);
 
         }
     }
+
     return (
         <main className="w-full">
-            {answer ? (<div className="flex flex-col items-center">{answer}</div>) :
+            {answer ? <div className={song.className}>
+                <div className="w-full mx-auto">
+                    <Image src="/assets/images/taro-card.png" alt="card" width={530} height={530} className="mx-auto sm:visible invisible" />
+                    <Image src="/assets/images/taro-6.jpeg" alt="card" width={530} height={530} className="opacity-20 mx-auto sm:invisible visible" />
+                </div>
+                <div className="w-full mx-auto">
+                </div>
+                <div className="absolute w-[75%] left-[13%] top-[20%] sm:top-[25%] sm:left-1/2 sm:w-[25%] sm:-translate-x-1/2 sm:-translate-y-1/2 sm:text-base items-center">
+                    <p className="mx-auto text-base sm:text-lg text-gray-600">{answer}</p>
+                </div>
+            </div> :
                 <>
                     {
                         questionArr.map((section: any, index: number) => {
                             if (currentIdx === index) {
                                 return (
-                                    <div key={index} className="pt-8">
-                                        <p className="font-semibold text-lg text-center">{index + 1}/{questionArr.length}</p>
-                                        <Card control={control} index={currentIdx} section={section} />
-                                        <div className="w-1/4 h-10 mx-auto grid grid-cols-6 justify-items-center -mt-12">
-                                            {index > 0 && <button onClick={() => goBack(index)} className="w-10 animate-fade-in rounded-full px-4 py-1.5 text-sm font-medium text-gray-500 transition-colors ease-out hover:text-black">&lt;</button>}
-                                            {index < questionArr.length - 1 && <button className="w-10 col-end-7 animate-fade-in rounded-full px-4 py-1.5 text-sm font-medium text-gray-500 transition-colors ease-out hover:text-black" onClick={() => onSwipe('right')}>{`>`}</button>}
+                                    <div key={index} className="pt-8 px-4">
+                                        <div className="mb-6 h-1 max-w-2xl mx-auto bg-neutral-200 items-center flex">
+                                            <div className={`h-1 bg-[#fde58a]`} style={{ width: `${((index + 1) / questionArr.length) * 100}%` }}></div>
+                                            <div className="w-5 border border-[#fbd24e] box-border bg-white aspect-square rounded-full -ml-2" />
+                                        </div>
+                                        <Card errors={errors} control={control} index={currentIdx} section={section} />
+                                        <div className="sm:max-w-2xl h-10 mx-auto grid grid-flow-col grid-cols-6 justify-items-center -mt-10 ">
+                                            <button onClick={() => goBack(index)} className={`${index <= 0 ? "opacity-50 pointer-events-none" : ""} col-span-3 col-start-1 col-end-4 bg-red-100 animate-fade-in rounded-l-lg w-full text-sm font-medium text-gray-500 transition-colors ease-out hover:text-black`}>&lt;</button>
+                                            <button className={`${inputBox ? "opacity-50 pointer-events-none" : ""} w-full col-span-3 col-start-4 col-end-7 animate-fade-in bg-blue-100 rounded-r-lg text-sm font-medium text-gray-500 transition-colors ease-out hover:text-black`} onClick={() => onSwipe(`${index < questionArr.length - 1 ? 'right' : 'result'}`)}>{`>`}</button>
                                         </div>
                                     </div>
                                 )
                             }
                         })
                     }
-                    {currentIdx === (questionArr.length - 1) && <div><div className="mx-auto w-full text-center p-6 space-y-2"><h3>추가로 작성하고싶은 고민을 입력하세요!</h3><textarea rows={5} className="rounded w-1/4 border border-gray-300" placeholder="없다면 빈칸으로 남겨두어도 좋습니다 :)" {...register("addText")} /></div><button className="mx-auto mb-8 black_btn" onClick={handleSubmit(onSubmit)}>결과보기</button></div>}
+                    {inputBox &&
+                        <section>
+                            <div className="mx-auto w-full text-center p-6 space-y-2">
+                                <h3>추가로 작성하고싶은 고민을 입력하세요!</h3>
+                                <textarea rows={5} className="focus:outline-none focus:ring-neutral-500 focus:ring-1 sm:max-w-2xl w-full rounded-md border border-neutral-400 p-4 text-neutral-900 shadow-sm placeholder:text-neutral-400 focus:border-neutral-900" placeholder="없다면 빈칸으로 남겨두어도 좋습니다 :)" {...register("addText")} />
+                            </div>
+                            <button className="mx-auto mb-8 black_btn" onClick={handleSubmit(onSubmit)}>
+                                결과보기
+                            </button>
+                        </section>}
                 </>
             }
-        </main>
+        </main >
     );
 }
 
