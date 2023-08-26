82cb64203ec2208619aed83bd4410532829a631d
HAEWON LEE <90237570+picapipicca@users.noreply.github.com>
2023-08-26 20:15:47 +0900

    new:config change

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    GTAG_ID: process.env.GTAG_ID,
+  }
 }
 
 module.exports = nextConfig
