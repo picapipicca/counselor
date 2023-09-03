3c1e43ea73f923a5d701e70093051a93945e9bfd
HAEWON LEE <90237570+picapipicca@users.noreply.github.com>
2023-09-03 21:15:45 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const debug = process.env.NODE_ENV !== "production";
-const repository = "counselor"
 
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
   reactStrictMode: true,
-  assetPrefix: !debug ? `/${repository}/` : "",
-  trailingSlash: true,
   env: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     GTAG_ID: process.env.GTAG_ID,
